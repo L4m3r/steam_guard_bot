ca96c13721ac954fef7fd8ed29bc4556a46da71d
Klimov Kirill <klimov.lamer@gmail.com>
2022-10-06 22:30:20 +0300

    Added telegram bot w/ MySQL database

---
+++ bot.py
@@ -0,0 +1,118 @@
+from selectors import EpollSelector
+import telebot
+from db.mysql_db import DB
+from steam_totm.guard import Guard
+
+db = DB('localhost', 'user', 'password', 'steam_guard')
+
+bot = telebot.TeleBot('5429350887:AAEr14yYCLa737PvBxj0q1So3fmv2en54xw')
+
+user_secret_name = {}
+user_step = {}
+
+def get_user_step(user_id):
+    if user_id in user_step:
+        return user_step[user_id]
+    else:
+        user_step[user_id] = 0
+        return 0
+
+@bot.message_handler(func=lambda message: message.text in ['/start', 'Назад'])
+def start(message):
+    markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    markup.row('Все аккаунты', 'Добавить аккаунт')
+    markup.row('Удалить аккаунт')
+    bot.send_message(message.chat.id, 'Бот', reply_markup=markup)
+    
+    user_step[message.chat.id] = 0
+
+@bot.message_handler(func=lambda message: message.text in ['/add', 'Добавить аккаунт'])
+def add(message):
+    bot.send_message(message.chat.id, 'Введите название аккаунта')
+    bot.register_next_step_handler(message, get_name)
+
+def get_name(message):
+    name = message.text
+    
+    if name == 'exit':
+        start(message)
+        return
+    
+    if db.is_exist(message.chat.id, name):
+        bot.send_message(message.chat.id, 'Название уже занято!\nПопробуйте другое или напишите exit что-бы выйти.')
+        bot.register_next_step_handler(message, get_name)
+        return
+    
+    user_secret_name[message.chat.id] = name
+    bot.send_message(message.chat.id, 'Введите секретный ключ')
+    bot.register_next_step_handler(message, get_secret)
+    
+def get_secret(message):
+    secret = message.text
+    
+    user_id = message.chat.id
+    
+    res = db.set_secret(user_id, user_secret_name[user_id], secret)
+    
+    if res.code:
+        bot.send_message(user_id, res.message)
+        bot.register_next_step_handler(message, get_secret)
+    
+    bot.send_message(user_id, 'Аккаунт успешно добавлен')
+
+@bot.message_handler(func=lambda message: message.text in ['/list', 'Все аккаунты'])
+def list(message):
+    names = db.get_user_secrets_name(message.chat.id)
+    
+    if names is []:
+        bot.send_message(message.chat.id, 'У вас нет добавленных аккаунтов')
+        return
+    
+    markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    for name in names:
+        markup.row(name)
+    
+    markup.row('Назад')
+        
+    bot.send_message(message.chat.id, 'Лист', reply_markup=markup)
+    user_step[message.chat.id] = 2
+
+@bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 2)
+def guard_code(message):
+    name = message.text
+    secret = db.get_secret(message.chat.id, name)
+    guard = Guard(secret)
+    code = guard.get_code()
+    bot.send_message(message.chat.id, code)
+
+@bot.message_handler(func=lambda message: message.text in ['/delete', 'Удалить аккаунт'])
+def delete_list(message):
+    names = db.get_user_secrets_name(message.chat.id)
+    
+    if names is []:
+        bot.send_message(message.chat.id, 'У вас нет добавленных аккаунтов')
+        return
+    
+    markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    for name in names:
+        markup.row(name)
+    
+    markup.row('Назад')
+        
+    bot.send_message(message.chat.id, 'Лист', reply_markup=markup)
+    user_step[message.chat.id] = 1
+
+@bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 1)
+def delete(message):
+    name = message.text
+    
+    res = db.delete_secret(message.chat.id, name)
+    if res.code:
+        bot.send_message(message.chat.id, res.message)
+        bot.register_next_step_handler(message, delete)
+        return
+    
+    bot.send_message(message.chat.id, 'Аккаунт успешно удалён')
+    user_step[message.chat.id] = 0
+
+bot.polling(non_stop=True, interval=0)
---
+++ db/mysql_db.py
@@ -0,0 +1,99 @@
+from enum import Enum
+from typing import Optional
+from mysql.connector import connect
+
+class Error(Enum):
+    OK = (0, 'Успешно')
+    SECRET_NAME_ALREADY_EXISTS = (1, 'Название уже занято')
+    LONG_NAME = (2, 'Название должно быть меньше 100 символов')
+    SECRET_DOES_NOT_EXISTS = (3, 'Данного аккаунта не существует')
+    
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+    
+
+class DB:
+    def __init__(
+            self, host: str, user: str,
+            password: str, database: str
+        ) -> None:
+        try:
+            self.connection = connect(
+                host=host,
+                user=user,
+                password=password,
+                database=database
+            )
+        except Exception as e:
+            print(e)
+            raise Exception('Can\'t connect to database')
+        
+    def __del__(self):
+        self.connection.close()
+        
+    def get_secret(self, user_id: int, name: str) -> Optional[str]:
+        query = """
+        SELECT secret 
+        FROM secrets
+        WHERE user_id = %s AND name = %s
+        """
+        
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, (user_id, name,))
+            
+            secret = cursor.fetchone()
+            return None if secret is None else secret[0]
+        
+    def get_user_secrets_name(self, user_id: int) -> list[str]:
+        query = """
+        SELECT name 
+        FROM secrets
+        WHERE user_id = %s
+        """
+        
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, (user_id,))
+            
+            names = cursor.fetchall()
+            return [] if names is None else list(name[0] for name in names)
+        
+    def set_secret(self, user_id: int, name: str, secret: str) -> Error:
+        if (len(name) >= 100):
+            return Error.LONG_NAME
+        
+        secrets = self.get_user_secrets_name(user_id)
+        
+        if secrets is not None and name in secrets:
+            return Error.SECRET_NAME_ALREADY_EXISTS
+        
+        query = """
+        INSERT INTO secrets
+        VALUES (%s, %s, %s)
+        """
+        
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, (user_id, name, secret,))
+            self.connection.commit()
+            
+        return Error.OK
+    
+    def delete_secret(self, user_id: int, name: str) -> Error:
+        secret = self.get_secret(user_id, name)
+        if secret is None:
+            return Error.SECRET_DOES_NOT_EXISTS
+        
+        query = """
+        DELETE FROM secrets
+        WHERE user_id = %s AND name = %s
+        """
+        
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, (user_id, name,))
+            self.connection.commit()
+        
+        return Error.OK
+            
+    def is_exist(self, user_id: int, name: str) -> bool:
+        return self.get_secret(user_id, name) is not None
+            
