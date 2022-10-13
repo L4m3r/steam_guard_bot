1d39cf9497f9708f3bd68e18a11cf56043aa6305
Klimov Kirill <klimov.lamer@gmail.com>
2022-10-13 13:05:28 +0300

    Added webhook for getting updates

--- bot.py
+++ bot.py
@@ -1,21 +1,43 @@
 import telebot
+from telebot import types
 from db.mysql_db import DB
 from steam_totm.guard import Guard
 import os
 from dotenv import load_dotenv, find_dotenv
-
+from aiohttp import web
 
 load_dotenv(find_dotenv())
 
+BOT_TOKEN = os.environ.get('BOT_TOKEN') or ''
+
+WEBHOOK_HOST = os.environ.get('WEBHOOK_HOST')
+WEBHOOK_PORT = os.environ.get('WEBHOOK_PORT')
+WEBHOOK_LISTEN = os.environ.get('WEBHOOK_LISTEN')
+
+# TODO add port from config
+WEBHOOK_URL_BASE = f"https://{WEBHOOK_HOST}:443"
+WEBHOOK_URL_PATH = f"/{BOT_TOKEN}/"
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+app = web.Application()
+
+async def handle(request):
+
+    request_body_dict = await request.json()
+    update = telebot.types.Update.de_json(request_body_dict)
+    bot.process_new_updates([update])
+    return web.Response(status=200)
+
+app.router.add_post('/{BOT_TOKEN}/', handle)
 
 db = DB(
-    os.environ.get('MYSQL_HOST'),
-    os.environ.get('MYSQL_USER'),
-    os.environ.get('MYSQL_PASSWORD'),
-    os.environ.get('MYSQL_DATABASE')
+    os.environ.get('MYSQL_HOST') or 'localhost',
+    os.environ.get('MYSQL_USER') or 'user',
+    os.environ.get('MYSQL_PASSWORD') or 'password',
+    os.environ.get('MYSQL_DATABASE') or 'steam_guard_bot'
 )
 
-bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
 
 user_secret_name = {}
 user_step = {}
@@ -28,7 +50,7 @@ def get_user_step(user_id):
         return 0
 
 @bot.message_handler(func=lambda message: message.text in ['/start', 'Назад'])
-def start(message):
+def start(message: types.Message):
     markup = telebot.types.ReplyKeyboardMarkup(True, False)
     markup.row('Все аккаунты', 'Добавить аккаунт')
     markup.row('Удалить аккаунт')
@@ -37,11 +59,11 @@ def start(message):
     user_step[message.chat.id] = 0
 
 @bot.message_handler(func=lambda message: message.text in ['/add', 'Добавить аккаунт'])
-def add(message):
+def add(message: types.Message):
     bot.send_message(message.chat.id, 'Введите название аккаунта')
     bot.register_next_step_handler(message, get_name)
 
-def get_name(message):
+def get_name(message: types.Message, content_types=['text']):
     name = message.text
     
     if name == 'exit':
@@ -57,7 +79,7 @@ def get_name(message):
     bot.send_message(message.chat.id, 'Введите секретный ключ')
     bot.register_next_step_handler(message, get_secret)
     
-def get_secret(message):
+def get_secret(message: types.Message, content_types=['text']):
     secret = message.text
     
     user_id = message.chat.id
@@ -71,7 +93,7 @@ def get_secret(message):
     bot.send_message(user_id, 'Аккаунт успешно добавлен')
 
 @bot.message_handler(func=lambda message: message.text in ['/list', 'Все аккаунты'])
-def list(message):
+def list(message: types.Message):
     names = db.get_user_secrets_name(message.chat.id)
     
     if names == []:
@@ -88,15 +110,18 @@ def list(message):
     user_step[message.chat.id] = 2
 
 @bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 2)
-def guard_code(message):
+def guard_code(message: types.Message):
     name = message.text
     secret = db.get_secret(message.chat.id, name)
+    if secret == None:
+        bot.send_message(message.chat.id, 'Код не найден')
+        return
     guard = Guard(secret)
     code = guard.get_code()
     bot.send_message(message.chat.id, code)
 
 @bot.message_handler(func=lambda message: message.text in ['/delete', 'Удалить аккаунт'])
-def delete_list(message):
+def delete_list(message: types.Message):
     names = db.get_user_secrets_name(message.chat.id)
     
     if names is []:
@@ -113,7 +138,7 @@ def delete_list(message):
     user_step[message.chat.id] = 1
 
 @bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 1)
-def delete(message):
+def delete(message: types.Message):
     name = message.text
     
     res = db.delete_secret(message.chat.id, name)
@@ -125,4 +150,12 @@ def delete(message):
     bot.send_message(message.chat.id, 'Аккаунт успешно удалён')
     user_step[message.chat.id] = 0
 
-bot.infinity_polling()
+bot.remove_webhook()
+
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
+
+web.run_app(
+    app,
+    host=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+)
